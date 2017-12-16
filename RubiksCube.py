b4dac966933c064824adbccb547ec31cd539f131
jkwon045 <32549643+jkwon045@users.noreply.github.com>
2017-12-15 22:39:26 -0800

    Add files via upload
    Added CCW Rotate to face class

--- RubiksCube.py
+++ RubiksCube.py
@@ -5,12 +5,16 @@ class Face:
             self.side.append([])
             for j in range (3):
                 self.side[i].append(num)
-    def out(self):
+
+    #prints the face
+    def _out(self):
         for i in range(3):
             for j in range(3):
                 print(self.side[i][j], end = ' ')
             print('\n')
-    def rotateCW(self):
+
+    #function to rotate the face clockwise
+    def _rotateCW(self):
         length = 2
         i = 0
         j = 0
@@ -24,37 +28,53 @@ class Face:
             bottomRow.append(self.side[2][x])
             leftCol.append(self.side[x][0])
             rightCol.append(self.side[x][2])
-            
-        #outputting every row
-        print("Top: ", end = "")
-        for x in range(3):
-            print(topRow[x], end = " ")
-        print("\nRight: ", end = " ")
-        for x in range(3):
-            print(rightCol[x], end = " ")
-        print("\nBottom: ", end = " ")
-        for x in range(3):
-            print(bottomRow[x], end = " ")
-        print("\nLeft: ", end = " ")
-        for x in range(3):
-            print(leftCol[x], end = " ")
-        print("\n")
         
-        #copy left column to top row
+        #copy left column to top row backwards
         while(i < length):
             self.side[0][i] = leftCol[length-i]
             i+=1
-        #copy top row to right column
+        #copy top row to right column forwards
         while(j < length):
             self.side[j][2] = topRow[j]
             j+=1
-        #copy right column to bottom row
+        #copy right column to bottom row backwards
         for x in reversed(range(3)):
-            self.side[2][x] = rightCol[length-i]
-        #copy bottom row to left column
+            self.side[2][x] = rightCol[length-x]
+        #copy bottom row to left column forwards
         for x in range(3):
             self.side[x][0] = bottomRow[x]
-    def changeCol(self, num, col):
+
+    #function to rotate face counter clockwise
+    def _rotateCCW(self):
+        length = 2
+        topRow = []
+        rightCol = []
+        bottomRow = []
+        leftCol = []
+
+        #copying off the rows to replace values later
+        for x in range(3):
+            topRow.append(self.side[0][x])
+            bottomRow.append(self.side[2][x])
+            leftCol.append(self.side[x][0])
+            rightCol.append(self.side[x][2])
+
+        #copy right column to top row forwards
+        for x in range(3):
+            self.side[0][x] = rightCol[x]
+        #copy bottom row to right column backwards
+        for x in reversed(range(3)):
+            self.side[x][2] = bottomRow[length-x]
+        #copy left column to bottom row
+        for x in range(3):
+            self.side[2][x] = leftCol[x]
+        #copy top row to left column backwards
+        for x in reversed(range(3)):
+            self.side[x][0] = topRow[length-x]
+
+        
+    #function to change a column in the face
+    def _changeCol(self, num, col):
         for i in range (3):
             self.side[i][col] = num
 
@@ -70,21 +90,25 @@ class Cube:
         
 
 def main():
-    print("Hello World")
     face = Face(0)
-    face.changeCol(1,0)
-    face.changeCol(2,2)
+    face._changeCol(1,0)
+    face._changeCol(2,2)
     print('intiial:')
-    face.out()
-    face.rotateCW()
-    print('rotate x1: ')
-    face.out()
-    face.rotateCW()
-    face.out()
-    face.rotateCW()
-    face.out()
-    face.rotateCW()
-    face.out()
-    face.rotateCW()
-    face.out()
+    face._out()
+
+    face._rotateCW()
+    print('rotate 1x: ')
+    face._out()
+    face._rotateCCW()
+    print('rotate 2z: ')
+    face._out()
+    face._rotateCW()
+    print('rotate 3z: ')
+    face._out()
+    face._rotateCCW()
+    print('rotate 4z: ')
+    face._out()
+    face._rotateCW()
+    print('rotate 5z: ')
+    face._out()
 main()
