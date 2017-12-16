81a646a44e229dcfd5d96efebfd22bd774c778bf
jkwon045 <32549643+jkwon045@users.noreply.github.com>
2017-12-16 00:16:50 -0800

    Added print function to Cube Class

--- RubiksCube.py
+++ RubiksCube.py
@@ -1,17 +1,29 @@
 class Face:
-    def __init__ (self,num):
+    def __init__ (self,char):
         self.side = []
         for i in range (3):
             self.side.append([])
             for j in range (3):
-                self.side[i].append(num)
+                self.side[i].append(char)
 
     #prints the face
     def _out(self):
+        visual = []
         for i in range(3):
             for j in range(3):
+                visual.append(self.side[i][j])
                 print(self.side[i][j], end = ' ')
             print('\n')
+        return visual
+
+    def _retRow(self, rowNum):
+        return self.side[rowNum]
+
+    def _retCol(self, colNum):
+        tmp = []
+        for i in range(3):
+            tmp.append[i][colNum]
+        return tmp
 
     #function to rotate the face clockwise
     def _rotateCW(self):
@@ -74,61 +86,79 @@ class Face:
 
         
     #function to change a column in the face
-    def _changeCol(self, *otherCol, colNum):
+    def _changeCol(self, otherCol, colNum):
         for i in range(3):
-            self.side[i][colNum] = otherCol[0][i]
+            self.side[i][colNum] = otherCol[i]
 
-    def _changeRow(self, *otherRow, rowNum):
+    def _changeRow(self, otherRow, rowNum):
         for i in range(3):
-            self.side[rowNum][i] = otherRow[0][i]
-
-    # functions to return column or row as an array
-    def _getCol(self, colNum):
-        col = []
-        for i in range(3):
-            self.side[i][colNum] = col[i]
-        return col
-
-    def _getRow(self, rowNum):
-        row = []
-        for i in range(3):
-            self.side[rowNum][i] = row[i]
-        return row
+            self.side[rowNum][i] = otherRow[i]
 
 class Cube:
     def __init__(self, char0, char1, char2, char3, char4, char5):
-        self.side0 = Face(char0) #front, orange
-        self.side1 = Face(char1) #top, yellow
-        self.side2 = Face(char2) #down, white
-        self.side3 = Face(char3) #left, green
-        self.side4 = Face(char4) #right, blue
-        self.side5 = Face(char5) #back, red
-    def rotateR(self):
-        self.side4._roateCW()
-        # save off columns of 4 faces (top, front, bottom, back)
-        uRightCol = self.side1._getCol(2)
-        fRightCol = self.side4._getCol(2)
-        dRightCol = self.side2._getCol(2)
-        bRightCol = self.side5._getCol(0) # maybe 2?
+        #front, orange
+        self.side0 = Face(char0)
+        #top, yellow
+        self.side1 = Face(char1)
+        #down, white
+        self.side2 = Face(char2)
+        #left, green
+        self.side3 = Face(char3)
+        #right, blue
+        self.side4 = Face(char4)
+        #back, red
+        self.side5 = Face(char5)
 
-        # assign columns (up->back->down->front->up)
-        self.side5._changeRow(uRightCol, 0) # maybe 2?
-        self.side2._changeRow(bRightCol, 2)
-        self.side4._changeRow(dRightCol, 2)
-        self.side1._changeRow(fRightCol, 2)
+    def _out(self):
+        dummyFace = Face(" ")
+        visual = []
+
+        #appending to visual representation
+
+        for i in range(3):
+            visual.append(dummyFace._retRow(rowNum=0))
+            visual.append(' ')
+            visual.append(self.side1._retRow(rowNum=i))
+            visual.append('\n')
+
+        for i in range(3):
+            visual.append(self.side3._retRow(rowNum = i))
+            visual.append(' ')
+            visual.append(self.side0._retRow(rowNum = i))
+            visual.append(' ')
+            visual.append(self.side4._retRow(rowNum = i))
+            visual.append(' ')
+            visual.append(self.side5._retRow(rowNum = i))
+            visual.append('\n')
+
+        for i in range(3):
+            visual.append(dummyFace._retRow(rowNum=0))
+            visual.append(' ')
+            visual.append(self.side2._retRow(rowNum=i))
+            visual.append('\n')
+
+        for i in range(len(visual)):
+            for j in range(len(visual[i])):
+                if ( j == len(visual[i])-1):
+                  print( visual[i][j], end = "" )
+                else:
+                    print(visual[i][j], end = " ")
+        
+        
+        
 
 def main():
     face = Face(0)
     dummy1 = [1, 1, 1]
     dummy2 = [2, 2, 2]
     face._changeCol(dummy1, colNum=0)
-    face._changeRow(dummy2, rowNum=2)
+    face._changeRow(dummy2, rowNum=1)
     print('intiial:')
     face._out()
 
     face._rotateCW()
     print('rotate 1x: ')
-    face._out()
+    print(face._out())
     face._rotateCCW()
     print('rotate 2z: ')
     face._out()
@@ -141,4 +171,7 @@ def main():
     face._rotateCW()
     print('rotate 5z: ')
     face._out()
+
+    c = Cube('a', 'b', 'c', 'd', 'e', 'f')
+    c._out()
 main()
